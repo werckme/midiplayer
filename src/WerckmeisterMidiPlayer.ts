7e95eeed2229556cf78d541441f30ea4ee6ddc19
Samba Godschynski <sambag@thinkpad.de>
2020-12-21 16:18:22 +0100

    some changes

--- src/WerckmeisterMidiPlayer.ts
+++ src/WerckmeisterMidiPlayer.ts
@@ -1,13 +1,23 @@
 
 export class WerckmeisterMidiPlayer {
     public async init(): Promise<void> {
+        await this.load();
         const audioContext = new AudioContext();
         try {
-            await audioContext.audioWorklet.addModule('WerckmeisterMidiPlayerAudioProcessor.js')
-            const audioNode = new AudioWorkletNode(audioContext, 'werckmeister-midi-player-audio-processor');
-            audioNode.connect(audioContext.destination)
+            //audioNode.connect(audioContext.destination)
         } catch (ex) {
             console.log(ex)
         }
+    }
+
+    async load() {
+        const url = 'https://werckme.github.io/midi-js-soundfonts/FluidR3_GM/acoustic_grand_piano-ogg.js';
+        const response = await fetch(url);
+        const text = await response.text();
+        const script:any = document.createElement('script');
+        script.language = 'javascript';
+        script.type = 'text/javascript';
+        script.text = text;
+        document.body.appendChild(script);
     }
 }
